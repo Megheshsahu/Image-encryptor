51fd83b0aea7b5631edf6cd2bcb318ecffd16018
Megheshsahu <megheshkumarsahu@gmail.com>
2025-07-21 02:32:55 +0530

    setting logics to  work on web

--- app.py
+++ app.py
@@ -41,11 +41,8 @@ def index():
     return render_template('index.html')
 
 
-# Run the app on Railway's expected host/port
-if __name__ == '__main__':
-    import os
-    port = int(os.environ.get('PORT', 8000))
-    app.run(host='0.0.0.0', port=port)
+
+# No need for __main__ block; Gunicorn will serve the app object directly
 
 
 
